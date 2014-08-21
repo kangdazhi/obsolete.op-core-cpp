c652a6853daba29f9acc0402d2908a04ab3a34e5
Bojan Lazic <lazic_bo@yahoo.com>
2014-08-21 17:56:52 +0200

    Updated Android makefile. https://github.com/openpeer/opandroid/issues/537

--- projects/android/Android.mk
+++ projects/android/Android.mk
@@ -47,18 +47,19 @@ LOCAL_SRC_FILES := $(SOURCE_PATH)/core_Account_ContactSubscription.cpp \
 		   $(SOURCE_PATH)/core_Cache.cpp \
 		   $(SOURCE_PATH)/core_Call.cpp \
 		   $(SOURCE_PATH)/core_CallTransport.cpp \
+		   $(SOURCE_PATH)/core_ComposingStatus.cpp \
 		   $(SOURCE_PATH)/core_Contact.cpp \
 		   $(SOURCE_PATH)/core_ConversationThread.cpp \
-		   $(SOURCE_PATH)/core_ConversationThreadComposingStatus.cpp \
 		   $(SOURCE_PATH)/core_ConversationThreadDocumentFetcher.cpp \
 		   $(SOURCE_PATH)/core_ConversationThreadHost.cpp \
 		   $(SOURCE_PATH)/core_ConversationThreadHost_PeerContact.cpp \
 		   $(SOURCE_PATH)/core_ConversationThreadHost_PeerLocation.cpp \
 		   $(SOURCE_PATH)/core_ConversationThreadSlave.cpp \
-		   $(SOURCE_PATH)/core_ConversationThreadSystemMessage.cpp \
-		   $(SOURCE_PATH)/core_Factory.cpp \
+		   $(SOURCE_PATH)/core_Decryptor.cpp \
+		   $(SOURCE_PATH)/core_Encryptor.cpp \
 		   $(SOURCE_PATH)/core_Helper.cpp \
 		   $(SOURCE_PATH)/core_Identity.cpp \
+		   $(SOURCE_PATH)/core_ISystemMessage.cpp \
 		   $(SOURCE_PATH)/core_IdentityLookup.cpp \
 		   $(SOURCE_PATH)/core_Logger.cpp \
 		   $(SOURCE_PATH)/core_Settings.cpp \
@@ -69,8 +70,8 @@ LOCAL_SRC_FILES := $(SOURCE_PATH)/core_Account_ContactSubscription.cpp \
 		   $(SOURCE_PATH)/core_PushMessaging.cpp \
 		   $(SOURCE_PATH)/core_PushMessaging_PushQuery.cpp \
 		   $(SOURCE_PATH)/core_PushMessaging_RegisterQuery.cpp \
-		   $(SOURCE_PATH)/../test/TestMediaEngine.cpp \
-		   $(SOURCE_PATH)/../test/boost_replacement.cpp
+		   #$(SOURCE_PATH)/../test/TestMediaEngine.cpp \
+		   #$(SOURCE_PATH)/../test/boost_replacement.cpp
 
 
 include $(BUILD_STATIC_LIBRARY)
