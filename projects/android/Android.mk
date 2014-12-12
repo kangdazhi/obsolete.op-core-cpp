6f41948bcefc545ef40e5b8c98dd2392cf087a68
Bojan Lazic <lazic_bo@yahoo.com>
2014-12-12 18:32:16 +0100

    Added op-core-cpp C++11 and NDK r10c support. https://github.com/openpeer/opandroid/issues/657

--- projects/android/Android.mk
+++ projects/android/Android.mk
@@ -2,16 +2,18 @@ LOCAL_PATH := $(call my-dir)/../../
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
+LOCAL_CLANG := true
 
 LOCAL_CFLAGS	:= -Wall \
 -W \
--std=gnu++11 \
 -O2 \
 -pipe \
 -fPIC \
 -frtti \
 -fexceptions \
 -D_ANDROID \
+
+LOCAL_CPPFLAGS += -std=c++11
 
 LOCAL_MODULE    := hfcore_android
 
@@ -21,21 +23,22 @@ LOCAL_C_INCLUDES:= $(LOCAL_PATH) \
 $(LOCAL_PATH)/openpeer/core/internal \
 $(LOCAL_PATH)/openpeer/core/test \
 $(LOCAL_PATH)/../ortc-lib/libs/zsLib \
-$(LOCAL_PATH)/../ortc-lib/libs/zsLib/internal \
+$(LOCAL_PATH)/../ortc-lib/libs/zsLib/zsLib/internal \
+$(LOCAL_PATH)/../ortc-lib/libs/zsLib/zsLib/extras \
 $(LOCAL_PATH)/../ortc-lib/libs/op-services-cpp \
 $(LOCAL_PATH)/../op-stack-cpp \
 $(LOCAL_PATH)/../ortc-lib/libs \
 $(LOCAL_PATH)/.. \
 $(LOCAL_PATH)/../ortc-lib/libs/build/android/curl/include \
 $(LOCAL_PATH)/../ortc-lib/libs/udns \
-$(LOCAL_PATH)../ortc-lib/libs/build/android/boost/include/boost-1_53 \
 $(LOCAL_PATH)/../ortc-lib/libs/webrtc \
 $(LOCAL_PATH)/../ortc-lib/libs/webrtc/webrtc \
 $(LOCAL_PATH)/../ortc-lib/libs/webrtc/webrtc/voice_engine/include \
 $(LOCAL_PATH)/../ortc-lib/libs/webrtc/webrtc/video_engine/include \
 $(LOCAL_PATH)/../ortc-lib/libs/webrtc/webrtc/modules/video_capture/include \
-$(ANDROIDNDK_PATH)/sources/cxx-stl/gnu-libstdc++/4.7/include \
-$(ANDROIDNDK_PATH)/sources/cxx-stl/gnu-libstdc++/4.7/libs/armeabi/include \
+$(ANDROIDNDK_PATH)/sources/android/support/include \
+$(ANDROIDNDK_PATH)/sources/cxx-stl/llvm-libc++/libcxx/include \
+$(ANDROIDNDK_PATH)/platforms/android-19/arch-arm/usr/include \
 
 SOURCE_PATH := openpeer/core/cpp
 
